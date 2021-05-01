f818cf1546a0858581fa451e80f0eaef1e24f401
merlin <wanipun.work@gmail.com>
2021-05-01 23:17:28 +0530

    added cookie policies

--- src/pages/cookie_accept_panel.js
+++ src/pages/cookie_accept_panel.js
@@ -1,26 +1,33 @@
-import React, { Component } from 'react';
-import Cookies from 'universal-cookie';
-const cookies = new Cookies('myCat');
-class CookieAccept
- extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    
-    render() { 
-        return (  <div className="cookie-accept container-fluid d-flex justify-content-center">
-                    <div className="row">
-                        <div className="col-xl-10 text-center text-md-left d-flex">
-                            <p className="my-auto">This website uses cookies to improve user experience. If you're continue browsing, we will consider that you accept their use</p>
-                        </div>
-                        <div className="col-xl-1 text-center mt-3 mt-md-0">
-                            <button className="btn btn-primary" onClick={() => this.props.set()}>ACCEPT</button>
-                        </div>
-                    </div>
-                </div>);
-    }
+import React, { Component } from "react";
+import Cookies from "universal-cookie";
+const cookies = new Cookies("myCat");
+class CookieAccept extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="cookie-accept container-fluid d-flex justify-content-center">
+        <div className="row">
+          <div className="col d-flex">
+            <p className="my-auto">
+              This website uses cookies to improve user experience. If you're
+              continue browsing, we will consider that you accept to our <a target="_blank" href="/cookie">Cookie Policies</a>
+            </p>
+          </div>
+          <div className="col text-center mt-3 mt-md-0">
+            <button
+              className="btn"
+              onClick={() => this.props.set()}>
+              ACCEPT
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
-export default CookieAccept
-;
+
+export default CookieAccept;
